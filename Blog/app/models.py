eddbff6ed00ec60bdd5a43b205773f98a33d1071
João Victor Santa <116450205+JoaoVictorSanta@users.noreply.github.com>
2024-11-12 01:16:47 -0300

    Add files via upload

---
+++ Blog/app/models.py
@@ -0,0 +1,25 @@
+﻿"""
+Definition of models.
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here...
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nome
+    
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    data_atualizacao = models.DateTimeField(auto_now=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.titulo
---
+++ Blog/app/views.py
@@ -0,0 +1,52 @@
+﻿from django.shortcuts import render, get_object_or_404
+from app.models import Post
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+
+def lista_posts(request):
+    posts = Post.objects.all()
+    return render(request, 'app/lista_posts.html', {'posts': posts})
+
+def detalhe_post(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'app/detalhe_post.html', {'post': post})
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
